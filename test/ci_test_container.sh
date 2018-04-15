8af7ccdd322bd39fa6a5d339d97fd1536c5166a3
Massimiliano Pasquesi <massipasquesi@gmail.com>
2018-04-15 16:57:09 +0200

    Improved CI_TEST_CMD execution

--- test/ci_test_container.sh
+++ test/ci_test_container.sh
@@ -1,14 +1,23 @@
 #!/bin/sh
-set -e
+set -em
+
+is_in_cmd()
+{
+    echo "$CI_TEST_CMD" | grep "$1" > /dev/null
+    return $?
+}
 
 if [ -n "$CI_TEST_CMD" ]; then
-    printf "\e[0;33m Gonna run TEST_CMD: %s \e[0m\n" "$CI_TEST_CMD"
-    $CI_TEST_CMD || exit $?
-fi
-
-if [ -n "$CI_TEST_SCRIPT" ] && [ -x "$CI_TEST_SCRIPT" ]; then
-    printf "\e[0;33m Gonna run TEST_SCRIPT: %s \e[0m\n" "$CI_TEST_SCRIPT"
-    $CI_TEST_SCRIPT || exit $?
+    if result=$(eval "$CI_TEST_CMD" 2>&1); then
+        printf '\e[42;1;97m [SUCCESS] %b \e[0m\n' "$result"
+        # If 'exit' in cmd do exit;
+        is_in_cmd "exit" && exit 0;
+    else
+        status=$?
+        # Because 'grep' do not print any message if 'pattern not found'
+        is_in_cmd "grep" && [ $status -eq 1 ] && result="grep: Pattern Not Found"
+        printf "\e[41;1;37m [FAILED] CODE: %d\n MESSAGE: %s \e[0m\n" $status "$result" && exit $((status));
+    fi
 fi
 
 exit 0;
