3dd8d0aef245d335b36a5eeee460e7557478858d
Massimiliano Pasquesi <massipasquesi@gmail.com>
2018-07-06 15:56:48 +0200

    Fixed shellcheck warnings

--- test/ci_test_container.sh
+++ test/ci_test_container.sh
@@ -8,7 +8,7 @@ is_in_cmd()
 }
 
 if [ -n "$CI_TEST_CMD" ]; then
-    printf "\e[47;1;35m [RUN TEST_CMD]: %s \e[0m\n" "$CI_TEST_SCRIPT"
+    printf '\e[47;1;35m [RUN TEST_CMD]: %s \e[0m\n' "$CI_TEST_SCRIPT"
 
     if result=$(eval "$CI_TEST_CMD" 2>&1); then
         printf '\e[42;1;97m [SUCCESS] Message:\e[0m\n \e[0;32m %b \n\e[42;1;97m[END]\e[0m\n' "$result"
@@ -18,7 +18,7 @@ if [ -n "$CI_TEST_CMD" ]; then
         status=$?
         # Because 'grep' do not print any message if 'pattern not found'
         is_in_cmd "grep" && [ $status -eq 1 ] && result="grep: Pattern Not Found"
-        printf "\e[41;1;37m [FAILED] CODE: %d\n MESSAGE: %s \e[0m\n" $status "$result" && exit $((status));
+        printf '\e[41;1;37m [FAILED] CODE: %d\n MESSAGE: %s \e[0m\n' $status "$result" && exit $((status));
     fi
 fi
 
